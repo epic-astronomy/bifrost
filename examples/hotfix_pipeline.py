9c255e685f01b73beddec0f97067c08cd3984866
mdtmc <miles.cranmer@gmail.com>
2016-08-02 17:01:32 -0700

    Working use of MitchCal solve and apply in a pipeline

--- examples/hotfix_pipeline.py
+++ examples/hotfix_pipeline.py
@@ -206,7 +206,6 @@ class FakeeVisBlock(TransformBlock):
 	# Then assign stand numbers.
 	inner = np.array([ x for x in uvw_data if abs(x[2]) < 50 and abs(x[3]) < 50 ])
 	uvw_data = np.append(inner, uvw_data, axis=0)[:n_baseline]
-
 	# Assign stands
 	for i in range(len(uvw_data)):
 	    uvw_data[i][0] = baselines[i][0]
@@ -216,43 +215,27 @@ class FakeeVisBlock(TransformBlock):
             1, self.num_stands, 
             2, self.num_stands, 
             2]).astype(np.complex64)
-        uv = []
-
+        #uv are given for each stand
+        uv = np.zeros(shape=[
+            self.num_stands, self.num_stands, 2])
         for row in uvw_data:
             stand1 = int(row[0])
             stand2 = int(row[1])
-
-	    if stand1 != stand2:
-              uv.append([stand1, stand2, row[2], row[3]])
-              uv.append([stand2, stand1, -row[2], -row[3]])
-
-              uvw_matrix[0, stand1, 0, stand2, 0] = row[4]+1j*row[5]
-
-              uvw_matrix[0, stand2, 0, stand1, 0] = row[4]-1j*row[5]
- 
-	# List the UV co-ords and the visibility
-        for i in range(len(uv)):
-	    stand1 = uv[i][0]
-	    stand2 = uv[i][1]
-            if abs(uvw_matrix[0, stand1, 0, stand2, 0]) > 0: print uv[i][2], uv[i][3], uvw_matrix[0, stand1, 0, stand2, 0]
-
+            uv[stand1, stand2, :] = [row[2], row[3]]
+            uv[stand2, stand1, :] = [-row[2], -row[3]]
+            uvw_matrix[0, stand1, 0, stand2, 0] = row[4]+1j*row[5]
+            uvw_matrix[0, stand1, 1, stand2, 1] = row[4]+1j*row[5]
+            uvw_matrix[0, stand2, 0, stand1, 0] = row[4]-1j*row[5]
+            uvw_matrix[0, stand2, 1, stand1, 1] = row[4]-1j*row[5]
         uv = np.array(uv).astype(np.float32)
         self.out_gulp_size = uvw_matrix.nbytes
-        self.header = json.dumps({
-            'dtype':str(np.complex64), 
-            'nbit':64,
-            'shape':uvw_matrix.shape})
         self.output_header = json.dumps({
             'dtype':str(np.complex64), 
             'nbit':64,
             'shape':uvw_matrix.shape})
-        span_generator = self.iterate_ring_write(output_rings[0])
-        span = span_generator.next()
-        span.data_view(np.complex64)[0][:] = uvw_matrix.ravel()
-        self.header = json.dumps({
-            'dtype':str(np.float32), 
-            'nbit':32,
-            'shape':uv.shape})
+        visibility_span_generator = self.iterate_ring_write(output_rings[0])
+        visibility_span = visibility_span_generator.next()
+        visibility_span.data_view(np.complex64)[0][:] = uvw_matrix.ravel()
         self.output_header = json.dumps({
             'dtype':str(np.float32), 
             'nbit':32,
@@ -260,8 +243,7 @@ class FakeeVisBlock(TransformBlock):
         self.out_gulp_size = uv.nbytes
         uv_span_generator = self.iterate_ring_write(output_rings[1])
         uv_span = uv_span_generator.next()
-        uv_span.data_view(np.float32)[0][:] = uv.ravel()
-
+        uv_span.data_view(np.float32)[0][:] = uv.ravel().astype(np.float32)
 blocks = []
 jones = 1*np.ones(shape=[
     1, 2, N_STANDS, 2]).astype(np.complex64)
@@ -270,17 +252,33 @@ jones[0, 1, :, 0] = np.zeros(N_STANDS)[:].astype(np.complex64)
 flags = 2*np.ones(shape=[
     1, N_STANDS]).astype(np.int8)
 blocks.append((FakeeVisBlock("mona_uvw.dat", N_STANDS), [], ['uncalibrated', 'uv_coords']))
-#blocks.append((FakeeVisBlock("mona_uvw.dat", N_STANDS), [], ['perfect', 0]))
-#blocks.append((TestingBlock(jones), [], ['jones_in']))
-#blocks.append((GainSolveBlock(flags, max_iterations=2000), ['uncalibrated', 'uncalibrated', 'jones_in'], ['model_out', 'jones_out']))
-blocks.append((WriteAsciiBlock('model_out.txt'), ['uncalibrated'], []))
+blocks.append((TestingBlock(jones), [], ['jones_in']))
+blocks.append((GainSolveBlock(flags, max_iterations=2000), ['uncalibrated', 'uncalibrated', 'jones_in'], ['model_out', 'jones_out']))
+blocks.append((WriteAsciiBlock('model_out.txt'), ['model_out'], []))
 blocks.append((WriteAsciiBlock('uv_coords.txt'), ['uv_coords'], []))
 Pipeline(blocks).main()
 output_visibilities = np.loadtxt('model_out.txt', dtype=np.float32).view(np.complex64).reshape((N_STANDS, 2, N_STANDS, 2))
 scalar_visibilities = output_visibilities[:, 0, :, 0]
-uv_points = np.loadtxt('uv_coords.txt', dtype=np.float32).reshape((N_STANDS*N_STANDS, 4))[:, 2:]
+uv_points = np.loadtxt('uv_coords.txt', dtype=np.float32).reshape((N_STANDS, N_STANDS, 2))
+data_to_grid = np.zeros(shape=[N_STANDS, N_STANDS, 4])
+for i in range(N_STANDS):
+    for j in range(N_STANDS):
+        data_to_grid[i, j] = np.array([
+            uv_points[i, j, 0],
+            uv_points[i, j, 1],
+            np.real(scalar_visibilities[i, j]), 
+            np.imag(scalar_visibilities[i, j])])
+new_blocks = []
+new_blocks.append((TestingBlock(data_to_grid), [], ['viz']))
+new_blocks.append((NearestNeighborGriddingBlock((512,512)), ['viz'], ['gridded']))
+new_blocks.append((IFFT2Block(), ['gridded'], ['ifftd']))
+new_blocks.append((WriteAsciiBlock('ifftd.txt'), ['ifftd'], []))
+Pipeline(new_blocks).main()
+dirty_image = np.abs(np.loadtxt('ifftd.txt', dtype=np.float32).view(np.complex64).reshape((512, 512)))
+from matplotlib import image
+image.imsave('is_this_mona.png', dirty_image, cmap='gray')
 
-
+"""
 visibilities = np.array([np.linalg.det(
     np.array([
     [output_visibilities[i, 0, j, 0], output_visibilities[i, 0, j, 1]],
@@ -298,16 +296,8 @@ for row in uv_points:
 gridding_feed = np.array(gridding_feed)
 
 
+"""
 
-new_blocks = []
-new_blocks.append((TestingBlock(gridding_feed), [], ['viz']))
-new_blocks.append((NearestNeighborGriddingBlock((250,250)), ['viz'], ['gridded']))
-new_blocks.append((IFFT2Block(), ['gridded'], ['ifftd']))
-new_blocks.append((WriteAsciiBlock('ifftd.txt'), ['ifftd'], []))
-Pipeline(new_blocks).main()
-dirty_image = np.abs(np.loadtxt('ifftd.txt', dtype=np.float32).view(np.complex64).reshape((250, 250)))
-from matplotlib import image
-image.imsave('is_this_mona.png', dirty_image)
 
 """
 bad_stands = [ 0,56,57,58,59,60,61,62,63,72,74,75,76,77,78,82,83,84,85,86,87,91,92,93,104,120,121,122,123,124,125,126,127,128,145,148,157,161,164,168,184,185,186,187,188,189,190,191,197,220,224,225,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255 ]
