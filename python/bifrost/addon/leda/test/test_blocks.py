599d8b46d7d10cab8a2f1ac8501f9b0b96beccf5
mdtmc <miles.cranmer@gmail.com>
2016-08-05 06:49:08 -0700

    Adding simple tests for new dada read block

--- python/bifrost/addon/leda/test/test_blocks.py
+++ python/bifrost/addon/leda/test/test_blocks.py
@@ -56,6 +56,22 @@ class TestDadaBlock(unittest.TestCase):
         Pipeline(self.blocks).main() 
         test_bytes = open(logfile, 'r').read(500).split(' ')
         self.assertAlmostEqual(np.float(test_bytes[0]), 3908.5, 3)
+class TestNewDadaReadBlock(unittest.TestCase):
+    """Test the ability of the Dada block to read
+        in data that is compatible with other blocks."""
+    def test_read_and_write(self):
+        """Reads in a dada file, and logs in ascii
+            file."""
+        logfile = '.log.txt'
+        self.blocks = []
+        self.blocks.append(
+            (NewDadaReadBlock(
+                "/data1/mcranmer/data/real/leda/2016_xaa.dada"),
+            [], [0]))
+        self.blocks.append((WriteAsciiBlock(logfile), [0], []))
+        Pipeline(self.blocks).main() 
+        test_bytes = open(logfile, 'r').read(500).split(' ')
+        self.assertAlmostEqual(np.float(test_bytes[0]), 3908.5, 3)
 
 if __name__ == "__main__":
     unittest.main()
